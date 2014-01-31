02f305235057592fb1bbe9b9159cbb3d1065e150
Andreas Sahle <andreas.sahle@wunderkraut.com>
2014-01-31 13:58:39 +0100

    add basic grunt config and stm submodule

---
+++ Gruntfile.js
@@ -0,0 +1,84 @@
+/**
+ * User: Andreas Sahle | @pixelmord
+ * Date: 31.01.14
+ * Time: 12:57
+ */
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+    // Project settings
+    yeoman: {
+      // configurable paths
+      app: 'steps2make/client',
+      dist: 'www'
+    },
+
+    // Make sure code styles are up to par and there are no obvious mistakes
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'Gruntfile.js',
+        '<%= yeoman.app %>/scripts/{,*/}*.js'
+      ],
+      test: {
+        options: {
+          jshintrc: 'test/.jshintrc'
+        },
+        src: ['test/spec/{,*/}*.js']
+      }
+    },
+
+    // Empties folders to start fresh
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '<%= yeoman.dist %>/*',
+            '!<%= yeoman.dist %>/.git*'
+          ]
+        }]
+      }
+    },
+
+    // Copies remaining files to places other tasks can use
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= yeoman.app %>',
+          dest: '<%= yeoman.dist %>',
+          src: [
+            '*.{ico,png,txt}',
+            '.htaccess',
+            '*.html',
+            'views/{,*/}*.html',
+            'components/**/*',
+            'images/{,*/}*.{webp}',
+            'fonts/*'
+          ]
+        }]
+      }
+    }
+  });
+
+  grunt.registerTask('default', [
+    'jshint',
+    'clean',
+    'copy'
+  ]);
+};
